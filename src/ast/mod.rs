17710c4655c6e506839754a89322f64f7971a869
msix29 <abdulrehmanessam7567@gmail.com>
2024-03-17 14:44:16 +0300

    Format code

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,20 +1,26 @@
 pub mod location;
 pub mod position;
+pub mod type_definition;
 pub mod value;
 pub mod variable_declaration;
-pub mod type_definition;
 
 use std::fmt::Display;
 use tree_sitter::{Node, TreeCursor};
 
-use self::{location::Location, type_definition::TypeDefinition, variable_declaration::VariableDeclaration};
+use self::{
+    location::Location, type_definition::TypeDefinition, variable_declaration::VariableDeclaration,
+};
 
 pub trait HasRawValue: Display {
     fn get_raw_value(&self) -> String;
 }
 
 pub trait AstNode: HasRawValue + Sized {
-    fn try_from_node<'a>(node: Node<'a>, cursor: &mut TreeCursor<'a>, code_bytes: &[u8]) -> Option<Vec<Self>>;
+    fn try_from_node<'a>(
+        node: Node<'a>,
+        cursor: &mut TreeCursor<'a>,
+        code_bytes: &[u8],
+    ) -> Option<Vec<Self>>;
 }
 
 pub trait HasLocation: AstNode {
