22e7540eae8193cb62ac8da6766e7f3107c740d4
msix29 <abdulrehmanessam7567@gmail.com>
2024-03-17 08:02:36 +0300

    Add simple value

---
+++ src/ast/value/simple.rs
@@ -0,0 +1,20 @@
+use std::fmt::Display;
+
+use crate::ast::HasRawValue;
+
+#[derive(Clone, Debug)]
+pub struct SimpleValue {
+    pub value: String,
+}
+
+impl Display for SimpleValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.get_raw_value())
+    }
+}
+
+impl HasRawValue for SimpleValue {
+    fn get_raw_value(&self) -> String {
+        self.value.to_string()
+    }
+}
