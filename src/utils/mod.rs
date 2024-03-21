332dc5b160a9e96cf1b09505204d9864a3a88780
msix29 <abdulrehmanessam7567@gmail.com>
2024-03-21 12:20:07 +0300

    Clean code

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,22 +1,32 @@
 use tree_sitter::Node;
 
+fn get_text_from_bytes(bytes: &[u8], start: usize, end: usize) -> String {
+    std::str::from_utf8(&bytes[start..end]).unwrap().to_string()
+}
+
 /// Gets spaces before and after a **token**. This function assumes this token has a parent
 /// as it's only called for individual tokens (ex. `local` in `local foo`).
 pub fn get_spaces(node: Node, code_bytes: &[u8]) -> (String, String) {
     let before = if let Some(before) = node.prev_sibling() {
-        std::str::from_utf8(&code_bytes[before.end_byte()..node.start_byte()]).unwrap()
+        // Leading spaces
+        get_text_from_bytes(code_bytes, before.end_byte(), node.start_byte())
     } else if let Some(before_parent) = node.parent().unwrap().prev_sibling() {
-        std::str::from_utf8(&code_bytes[before_parent.end_byte()..node.start_byte()]).unwrap()
+        // Leading spaces for parent
+        get_text_from_bytes(code_bytes, before_parent.end_byte(), node.start_byte())
     } else {
-        std::str::from_utf8(&code_bytes[..node.start_byte()]).unwrap()
+        // Leading spaces from the start of the file
+        get_text_from_bytes(code_bytes, 0, node.start_byte())
     };
 
     let after = if let Some(next) = node.next_sibling() {
-        std::str::from_utf8(&code_bytes[node.end_byte()..next.start_byte()]).unwrap()
+        // Trailing spaces
+        get_text_from_bytes(code_bytes, node.end_byte(), next.start_byte())
     } else if let Some(next_parent) = node.parent().unwrap().next_sibling() {
-        std::str::from_utf8(&code_bytes[node.end_byte()..next_parent.start_byte()]).unwrap()
+        // Trailing spaces for parent
+        get_text_from_bytes(code_bytes, node.end_byte(), next_parent.start_byte())
     } else {
-        std::str::from_utf8(&code_bytes[..node.start_byte()]).unwrap()
+        // Trailing spaces till the end of the file
+        get_text_from_bytes(code_bytes, 0, node.start_byte())
     };
 
     (before.to_string(), after.to_string())
