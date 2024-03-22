6ecc8fdfc9fea195efd7bd4816d3a4d046a7bf83
msix29 <abdulrehmanessam7567@gmail.com>
2024-03-22 05:42:53 +0300

    Fix trailing spaces at the end of the node

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -26,7 +26,7 @@ pub fn get_spaces(node: Node, code_bytes: &[u8]) -> (String, String) {
         get_text_from_bytes(code_bytes, node.end_byte(), next_parent.start_byte())
     } else {
         // Trailing spaces till the end of the file
-        get_text_from_bytes(code_bytes, 0, node.start_byte())
+        get_text_from_bytes(code_bytes, node.start_byte(), code_bytes.len() - 1)
     };
 
     (before.to_string(), after.to_string())
