ad61b19ab28fb0da8a16d0a93c34ed4ce50516a8
msix29 <abdulrehmanessam7567@gmail.com>
2024-03-28 11:31:11 +0300

    Edit local assignments

--- src/ast/variable_declaration.rs
+++ src/ast/variable_declaration.rs
@@ -3,8 +3,7 @@
 use tree_sitter::{Node, TreeCursor};
 
 use crate::prelude::{
-    AstNode, Expression, ExpressionInner, List, ListItem, LocalAssignment, NormalizedName,
-    SingleToken,
+    AstNode, Expression, ExpressionInner, List, LocalAssignment, NormalizedName, SingleToken,
 };
 
 impl AstNode for LocalAssignment {
@@ -24,31 +23,14 @@ impl AstNode for LocalAssignment {
         let all_names = node
             .children_by_field_name("binding", cursor)
             .collect::<Vec<Node>>();
-        let separators = node
-            .children_by_field_name("separator", cursor)
-            .collect::<Vec<Node>>();
-
-        let names = all_names
-            .iter()
-            .enumerate()
-            .map(|(i, binding)| {
-                if let Some(separator) = separators.get(i) {
-                    ListItem::Trailing {
-                        item: NormalizedName::from((binding.child(0).unwrap(), code_bytes)),
-                        separator: SingleToken::from((*separator, code_bytes)),
-                    }
-                } else {
-                    ListItem::NonTrailing(NormalizedName::from((
-                        binding.child(0).unwrap(),
-                        code_bytes,
-                    )))
-                }
-            })
-            .collect::<Vec<ListItem<NormalizedName>>>();
 
         Some(LocalAssignment {
             local_token: SingleToken::from((node.child(0).unwrap(), code_bytes)),
-            name_list: List { items: names },
+            name_list: List {
+                items: List::from_iter(all_names, node, "separator", code_bytes, |binding| {
+                    NormalizedName::from((binding.child(0).unwrap(), code_bytes))
+                }),
+            },
             equal_token: node
                 .child_by_field_name("equal")
                 .map(|equal| SingleToken::from((equal, code_bytes))),
