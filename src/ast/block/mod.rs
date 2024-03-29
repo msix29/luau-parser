bbecd4914c30b4469bd7970354ffc87910b6b143
msix29 <abdulrehmanessam7567@gmail.com>
2024-03-29 15:34:32 +0300

    Add files for all blocks

--- src/ast/block/mod.rs
+++ src/ast/block/mod.rs
@@ -1,4 +1,15 @@
 //! Module loading in helpful traits for block-related structs.
 
-pub mod if_statement;
-pub mod do_block;
+mod do_block;
+mod for_in;
+mod if_statement;
+mod numerical_for;
+mod repeat_block;
+mod while_loop;
+
+pub use do_block::*;
+pub use for_in::*;
+pub use if_statement::*;
+pub use numerical_for::*;
+pub use repeat_block::*;
+pub use while_loop::*;
--- src/types/shared_types/block/mod.rs
+++ src/types/shared_types/block/mod.rs
@@ -1,7 +1,15 @@
 //! Module holding all possible blocks in Luau (excluding functions).
 
-mod if_statement;
 mod do_block;
+mod for_in;
+mod if_statement;
+mod numerical_for;
+mod repeat_block;
+mod while_loop;
 
-pub use if_statement::*;
 pub use do_block::*;
+pub use for_in::*;
+pub use if_statement::*;
+pub use numerical_for::*;
+pub use repeat_block::*;
+pub use while_loop::*;
