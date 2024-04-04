161ae609505af2e63ed8649f689a415d057dee5f
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-04 05:06:54 +0300

    Move `is_in_bounds` to `Position`.

--- src/ast/location.rs
+++ src/ast/location.rs
@@ -1,6 +1,6 @@
 //! Implements helper traits for locations.
 
-use crate::prelude::{Location, Position};
+use crate::prelude::Location;
 
 impl Location {
     /// Offsets the whole location by lines and characters. For offsetting either start
@@ -8,17 +8,5 @@ impl Location {
     pub fn offset(&mut self, lines: i32, characters: i32) {
         self.start.offset(lines, characters);
         self.end.offset(lines, characters);
-    }
-
-    /// Checks whether or not the passed position is inside this location.
-    pub fn is_in_bounds(&self, position: &Position) -> bool {
-        let is_after_start = self.start.line == position.line
-            && self.start.character <= position.character
-            || self.start.line < position.line;
-        let is_before_end = self.end.line == position.line
-            && self.end.character >= position.character
-            || self.end.line > position.line;
-
-        is_after_start && is_before_end
     }
 }
--- src/ast/position.rs
+++ src/ast/position.rs
@@ -1,6 +1,6 @@
 //! Implements helper traits for positions.
 
-use crate::prelude::Position;
+use crate::prelude::{Location, Position};
 
 impl Position {
     /// Offsets the current position by lines and characters. If you're adding both lines
@@ -19,5 +19,17 @@ impl Position {
     /// Sets line to a specific value.
     pub fn set_character(&mut self, character: u32) {
         self.character = character;
+    }
+
+    /// Checks whether or not this position is inside the passed location.
+    pub fn is_in_bounds(&self, location: &Location) -> bool {
+        let is_after_start = location.start.line == self.line
+            && location.start.character <= self.character
+            || location.start.line < self.line;
+        let is_before_end = location.end.line == self.line
+            && location.end.character >= self.character
+            || location.end.line > self.line;
+
+        is_after_start && is_before_end
     }
 }
