f0e489e0a8f07bccd8cbe43251d1dfd12aec7f68
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-06 19:37:00 +0300

    Fix small issue

--- src/display/name.rs
+++ src/display/name.rs
@@ -5,7 +5,7 @@ use crate::prelude::{HasRawValue, NormalizedName};
 impl HasRawValue for NormalizedName {
     fn get_raw_value(&self) -> String {
         if let Some(r#type) = self.r#type {
-            format!("{}: {}", self.name.get_raw_value(), self.r#type.get_raw_value())
+            format!("{}: {}", self.name.get_raw_value(), r#type.get_raw_value())
         } else {
             self.name.get_raw_value()
         }
