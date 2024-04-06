4c46e0de9daac072dd386cd593149f411df271de
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-06 19:21:28 +0300

    Add hover for `NormalizedName`s

---
+++ src/display/name.rs
@@ -0,0 +1,13 @@
+//! Implements display traits for names.
+
+use crate::prelude::{HasRawValue, NormalizedName};
+
+impl HasRawValue for NormalizedName {
+    fn get_raw_value(&self) -> String {
+        if let Some(r#type) = self.r#type {
+            format!("{}: {}", self.name.get_raw_value(), self.r#type.get_raw_value())
+        } else {
+            self.name.get_raw_value()
+        }
+    }
+}
