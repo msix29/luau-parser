b8b208a810d5c1bebff698e834066429839c892d
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-08 19:39:43 +0300

    Refactor `HasRawValue` for `List<T>`s

--- src/display/list.rs
+++ src/display/list.rs
@@ -1,13 +1,18 @@
 //! Implements display traits for lists.
 
-use crate::prelude::{HasRawValue, List};
+use crate::prelude::{HasRawValue, List, ListItem};
 
 impl<T: HasRawValue> HasRawValue for List<T> {
     fn get_raw_value(&self) -> String {
         self.items
             .iter()
-            .map(|item| item.get_raw_value())
+            .map(|item| match item {
+                ListItem::Trailing { item, separator } => {
+                    format!("{}{}", item.get_raw_value(), separator.get_raw_value())
+                }
+                ListItem::NonTrailing(item) => item.get_raw_value(),
+            })
             .collect::<Vec<String>>()
-            .join(", ")
+            .join("\n")
     }
 }
