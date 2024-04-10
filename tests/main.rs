8c7f69e13a396c036efed0f9f61754fbf14f5401
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-10 21:38:11 +0300

    Add simple tests

---
+++ tests/main.rs
@@ -0,0 +1,23 @@
+#[cfg(test)]
+mod tests {
+    use luau_parser::prelude::*;
+
+    #[test]
+    /// Checks whether or not creating a parser is working.
+    fn creating_parser() {
+        LuauParser::new();
+    }
+
+    #[test]
+    /// Checks whether or not the length of the cache is correct.
+    fn cache_length() {
+        let mut parser = LuauParser::new();
+        assert_eq!(parser.get_cache().len(), 0);
+
+        parser.parse("", "test1");
+        assert_eq!(parser.get_cache().len(), 1);
+
+        parser.parse("", "test2");
+        assert_eq!(parser.get_cache().len(), 2);
+    }
+}
