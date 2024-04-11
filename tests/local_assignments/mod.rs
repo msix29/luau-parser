b790aa234bd26d3e722f1eddd9fc73ea3631c554
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-11 15:03:40 +0300

    Add more tests to local assignments

--- tests/local_assignments/mod.rs
+++ tests/local_assignments/mod.rs
@@ -1,12 +1,17 @@
+use std::sync::Arc;
+
 use luau_parser::{
     get_item_from_tuple_enum,
     prelude::LuauParser,
-    types::{HasRawValue, Statement},
+    types::{
+        ExpressionInner, FunctionArguments, FunctionCall, FunctionCallInvoked, HasRawValue, List,
+        Location, Position, PrefixExp, SingleToken, Statement, Var,
+    },
 };
 
 #[test]
 /// Tests simple local assignments.
-fn simple_local_assignment() {
+fn local_assignment_1() {
     let mut parser = LuauParser::new();
 
     let ast = parser.parse(r#"local foo = "Hello, World!""#, "1");
@@ -16,5 +21,78 @@ fn simple_local_assignment() {
     assert_eq!(assignment.name_list.items.len(), 1);
     assert_eq!(assignment.name_list.items[0].name.word, "foo");
     assert_eq!(assignment.expressions.items.len(), 1);
-    assert_eq!(assignment.expressions.items[0].get_raw_value(), r#""Hello, World!""#);
+    assert_eq!(
+        assignment.expressions.items[0].get_raw_value(),
+        r#""Hello, World!""#
+    );
+}
+
+#[test]
+/// Tests simple local assignments.
+fn local_assignment_2() {
+    let mut parser = LuauParser::new();
+
+    let ast = parser.parse(r#"local a, b, c = 1, foo()"#, "");
+    assert_eq!(ast.tokens.len(), 1);
+    assert!(matches!(ast.tokens[0], Statement::LocalAssignment(_)));
+    let assignment = get_item_from_tuple_enum!(&ast.tokens[0], Statement::LocalAssignment);
+    assert_eq!(assignment.name_list.items.len(), 3);
+    assert_eq!(assignment.name_list.get_raw_value(), "a,b,c");
+    assert_eq!(assignment.expressions.items.len(), 2);
+    assert_eq!(assignment.expressions.items[0].get_raw_value(), "1");
+    assert_eq!(
+        *assignment.expressions.items[1].inner,
+        ExpressionInner::FunctionCall(FunctionCall {
+            invoked: FunctionCallInvoked::Function(Arc::new(PrefixExp::Var(Var::Name(
+                SingleToken {
+                    spaces_before: " ".to_string(),
+                    word: "foo".to_string(),
+                    spaces_after: "".to_string(),
+                    location: Location {
+                        start: Position {
+                            line: 0,
+                            character: 19
+                        },
+                        end: Position {
+                            line: 0,
+                            character: 22,
+                        }
+                    }
+                }
+            )))),
+            arguments: FunctionArguments::List {
+                open_parenthesis: SingleToken {
+                    spaces_before: "".to_string(),
+                    word: "(".to_string(),
+                    spaces_after: "".to_string(),
+                    location: Location {
+                        start: Position {
+                            line: 0,
+                            character: 22
+                        },
+                        end: Position {
+                            line: 0,
+                            character: 23
+                        }
+                    }
+                },
+                arguments: List::default(),
+                close_parenthesis: SingleToken {
+                    spaces_before: "".to_string(),
+                    word: ")".to_string(),
+                    spaces_after: "".to_string(),
+                    location: Location {
+                        start: Position {
+                            line: 0,
+                            character: 23
+                        },
+                        end: Position {
+                            line: 0,
+                            character: 24
+                        }
+                    }
+                }
+            },
+        })
+    );
 }
