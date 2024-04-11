65c4e486e565e19beddfeb083b7e56b997d9c3ad
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-11 14:07:01 +0300

    Rename variables

--- tests/local_assignments/mod.rs
+++ tests/local_assignments/mod.rs
@@ -9,12 +9,12 @@ use luau_parser::{
 fn simple_local_assignment() {
     let mut parser = LuauParser::new();
 
-    let ast_1 = parser.parse(r#"local foo = "Hello, World!""#, "1");
-    assert_eq!(ast_1.tokens.len(), 1);
-    assert!(matches!(ast_1.tokens[0], Statement::LocalAssignment(_)));
-    let assignment_1 = get_item_from_tuple_enum!(&ast_1.tokens[0], Statement::LocalAssignment);
-    assert_eq!(assignment_1.name_list.items.len(), 1);
-    assert_eq!(assignment_1.name_list.items[0].name.word, "foo");
-    assert_eq!(assignment_1.expressions.items.len(), 1);
-    assert_eq!(assignment_1.expressions.items[0].get_raw_value(), r#""Hello, World!""#);
+    let ast = parser.parse(r#"local foo = "Hello, World!""#, "1");
+    assert_eq!(ast.tokens.len(), 1);
+    assert!(matches!(ast.tokens[0], Statement::LocalAssignment(_)));
+    let assignment = get_item_from_tuple_enum!(&ast.tokens[0], Statement::LocalAssignment);
+    assert_eq!(assignment.name_list.items.len(), 1);
+    assert_eq!(assignment.name_list.items[0].name.word, "foo");
+    assert_eq!(assignment.expressions.items.len(), 1);
+    assert_eq!(assignment.expressions.items[0].get_raw_value(), r#""Hello, World!""#);
 }
