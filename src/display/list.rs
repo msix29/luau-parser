4e815544879e074dc4c31838017f3a9ba3feecc1
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-12 19:24:35 +0300

    Refactor `List<T>` `impl`s

--- src/display/list.rs
+++ src/display/list.rs
@@ -2,32 +2,25 @@
 
 use crate::prelude::{HasRawValue, List, ListItem, Print};
 
-impl<T: HasRawValue> HasRawValue for List<T> {
-    fn get_raw_value(&self) -> String {
-        self.items
-            .iter()
-            .map(|item| match item {
-                ListItem::Trailing { item, separator } => {
-                    format!("{}{}", item.get_raw_value(), separator.get_raw_value())
-                }
-                ListItem::NonTrailing(item) => item.get_raw_value(),
-            })
-            .collect::<Vec<String>>()
-            .join("")
-    }
+/// Implement a `trait` for [`List<T>`] where `T: trait`.
+macro_rules! impl_list {
+    ($trait: ident, $fn_name: ident) => {
+        impl<T: $trait> $trait for List<T> {
+            fn $fn_name(&self) -> String {
+                self.items
+                    .iter()
+                    .map(|item| match item {
+                        ListItem::Trailing { item, separator } => {
+                            format!("{}{}", item.$fn_name(), separator.$fn_name())
+                        }
+                        ListItem::NonTrailing(item) => item.$fn_name(),
+                    })
+                    .collect::<Vec<String>>()
+                    .join("")
+            }
+        }
+    };
 }
 
-impl<T: Print> Print for List<T> {
-    fn print(&self) -> String {
-        self.items
-            .iter()
-            .map(|item| match item {
-                ListItem::Trailing { item, separator } => {
-                    format!("{}{}", item.print(), separator.print())
-                }
-                ListItem::NonTrailing(item) => item.print(),
-            })
-            .collect::<Vec<String>>()
-            .join("")
-    }
-}
+impl_list!(HasRawValue, get_raw_value);
+impl_list!(Print, print);
