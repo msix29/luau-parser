955708d723ad218ee66c70bd799731a203d6d797
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-12 14:28:43 +0300

    Implement `Print` for `SingleToken`

--- src/display/token.rs
+++ src/display/token.rs
@@ -1,9 +1,14 @@
 //! Implements display traits for single tokens.
 
-use crate::prelude::{HasRawValue, SingleToken};
+use crate::prelude::{HasRawValue, Print, SingleToken};
 
 impl HasRawValue for SingleToken {
     fn get_raw_value(&self) -> String {
         self.word.to_string()
+    }
+}
+impl Print for SingleToken {
+    fn print(&self) -> String {
+        format!("{}{}{}", self.spaces_before, self.word, self.spaces_after)
     }
 }
