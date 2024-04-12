3749836202bd6c397b1bf5493b8c777153dedd11
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-12 19:21:35 +0300

    Implement `Print` for `List<T: Print>`

--- src/display/list.rs
+++ src/display/list.rs
@@ -1,6 +1,6 @@
 //! Implements display traits for lists.
 
-use crate::prelude::{HasRawValue, List, ListItem};
+use crate::prelude::{HasRawValue, List, ListItem, Print};
 
 impl<T: HasRawValue> HasRawValue for List<T> {
     fn get_raw_value(&self) -> String {
@@ -11,6 +11,21 @@ impl<T: HasRawValue> HasRawValue for List<T> {
                     format!("{}{}", item.get_raw_value(), separator.get_raw_value())
                 }
                 ListItem::NonTrailing(item) => item.get_raw_value(),
+            })
+            .collect::<Vec<String>>()
+            .join("")
+    }
+}
+
+impl<T: Print> Print for List<T> {
+    fn print(&self) -> String {
+        self.items
+            .iter()
+            .map(|item| match item {
+                ListItem::Trailing { item, separator } => {
+                    format!("{}{}", item.print(), separator.print())
+                }
+                ListItem::NonTrailing(item) => item.print(),
             })
             .collect::<Vec<String>>()
             .join("")
