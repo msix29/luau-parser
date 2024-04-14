1de3881a69dbe8da98af1d297bb50584b9c368b3
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-14 07:11:57 +0300

    Fix `List` raw values

--- src/display/list.rs
+++ src/display/list.rs
@@ -1,26 +1,40 @@
 //! Implements display traits for lists.
-
 use crate::prelude::{HasRawValue, List, ListItem, Print};
 
 /// Implement a `trait` for [`List<T>`] where `T: trait`.
 macro_rules! impl_list {
-    ($trait: ident, $fn_name: ident) => {
+    ($trait: ident, $fn_name: ident, $join_with: literal) => {
         impl<T: $trait> $trait for List<T> {
             fn $fn_name(&self) -> String {
+                let len = self.items.len();
+                if len == 0 {
+                    return String::new();
+                }
+                let last_index = len - 1;
+
                 self.items
                     .iter()
-                    .map(|item| match item {
-                        ListItem::Trailing { item, separator } => {
-                            format!("{}{}", item.$fn_name(), separator.$fn_name())
+                    .enumerate()
+                    .map(|(i, item)| {
+                        let str = match item {
+                            ListItem::Trailing { item, separator } => {
+                                format!("{}{}", item.$fn_name(), separator.$fn_name())
+                            }
+                            ListItem::NonTrailing(item) => item.$fn_name(),
+                        };
+
+                        if i == last_index {
+                            str
+                        } else {
+                            str.trim_end().to_string()
                         }
-                        ListItem::NonTrailing(item) => item.$fn_name(),
                     })
                     .collect::<Vec<String>>()
-                    .join("")
+                    .join($join_with)
             }
         }
     };
 }
 
-impl_list!(HasRawValue, get_raw_value);
-impl_list!(Print, print);
+impl_list!(HasRawValue, get_raw_value, " ");
+impl_list!(Print, print, "");
