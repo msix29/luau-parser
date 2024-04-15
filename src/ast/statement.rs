9708104edefa61ac682ef3d0722217ea5f1ab65c
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-15 19:32:14 +0300

    Refactor some code for better reusability

--- src/ast/statement.rs
+++ src/ast/statement.rs
@@ -2,7 +2,11 @@
 
 use tree_sitter::Node;
 
-use crate::prelude::{Expression, LastStatement, SingleToken};
+use crate::prelude::{
+    Comment, CompoundSetExpression, DoBlock, Expression, FunctionCall, GenericFor, GlobalFunction,
+    IfStatement, LastStatement, LocalAssignment, LocalFunction, LuauStatement, NumericalFor,
+    RepeatBlock, SetExpression, SingleToken, Statement, TypeDefinition, WhileLoop,
+};
 
 impl From<(Node<'_>, &[u8])> for LastStatement {
     fn from((node, code_bytes): (Node, &[u8])) -> Self {
@@ -24,6 +28,68 @@ impl From<(Node<'_>, &[u8])> for LastStatement {
                 semicolon,
             },
             _ => unreachable!(),
+        }
+    }
+}
+
+impl From<(Node<'_>, &[u8])> for Statement {
+    fn from((statement, code_bytes): (Node, &[u8])) -> Self {
+        let mut cursor = statement.walk();
+        if let Some(variable_declaration) =
+            LocalAssignment::try_from_node(statement, &mut cursor, code_bytes)
+        {
+            Statement::LocalAssignment(variable_declaration)
+        } else if let Some(type_declaration) =
+            TypeDefinition::try_from_node(statement, &mut cursor, code_bytes)
+        {
+            Statement::TypeDefinition(type_declaration)
+        } else if let Some(if_statement) =
+            IfStatement::try_from_node(statement, &mut cursor, code_bytes)
+        {
+            Statement::IfStatement(if_statement)
+        } else if let Some(do_block) = DoBlock::try_from_node(statement, &mut cursor, code_bytes) {
+            Statement::DoBlock(do_block)
+        } else if let Some(generic_for) =
+            GenericFor::try_from_node(statement, &mut cursor, code_bytes)
+        {
+            Statement::GenericFor(generic_for)
+        } else if let Some(numerical_for) =
+            NumericalFor::try_from_node(statement, &mut cursor, code_bytes)
+        {
+            Statement::NumericalFor(numerical_for)
+        } else if let Some(repeat_block) =
+            RepeatBlock::try_from_node(statement, &mut cursor, code_bytes)
+        {
+            Statement::RepeatBlock(repeat_block)
+        } else if let Some(while_loop) =
+            WhileLoop::try_from_node(statement, &mut cursor, code_bytes)
+        {
+            Statement::WhileLoop(while_loop)
+        } else if let Some(set_expression) =
+            SetExpression::try_from_node(statement, &mut cursor, code_bytes)
+        {
+            Statement::SetExpression(set_expression)
+        } else if let Some(compound_set_expression) =
+            CompoundSetExpression::try_from_node(statement, &mut cursor, code_bytes)
+        {
+            Statement::CompoundSetExpression(compound_set_expression)
+        } else if let Some(function_call) =
+            FunctionCall::try_from_node(statement, &mut cursor, code_bytes)
+        {
+            Statement::FunctionCall(function_call)
+        } else if let Some(local_function) =
+            LocalFunction::try_from_node(statement, &mut cursor, code_bytes)
+        {
+            Statement::LocalFunction(local_function)
+        } else if let Some(global_function) =
+            GlobalFunction::try_from_node(statement, &mut cursor, code_bytes)
+        {
+            Statement::GlobalFunction(global_function)
+        } else if let Some(comment) = Comment::try_from_node(statement, &mut cursor, code_bytes) {
+            Statement::Comment(comment)
+        } else {
+            // Should be unreachable, but just to be sure, we won't continue the loop.
+            unreachable!();
         }
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -8,84 +8,21 @@ use std::sync::Arc;
 use tree_sitter::Tree;
 use tree_sitter::{Node, Parser};
 
-use crate::prelude::{
-    Ast, Comment, CompoundSetExpression, DoBlock, FunctionCall, GenericFor, GlobalFunction,
-    IfStatement, LastStatement, LocalAssignment, LocalFunction, LuauStatement, NumericalFor,
-    RepeatBlock, SetExpression, SingleToken, Statement, TypeDefinition, WhileLoop,
-};
+use crate::prelude::{Ast, LastStatement, SingleToken, Statement};
 
 /// Parses a code block and fills `tokens` with the parsed ones. The tokens can then
 /// be used to make the syntax tre.
 pub(crate) fn parse_block(body: &Node, code_bytes: &[u8], uri: Option<String>) -> Ast {
     let mut statements = Vec::new();
 
-    let mut cursor = body.walk();
     for i in 0..body.child_count() {
         let node = body.child(i).unwrap();
         if node.has_error() {
             continue;
         }
 
-        let statement = node.child(0).unwrap();
-        let token = if let Some(variable_declaration) =
-            LocalAssignment::try_from_node(statement, &mut cursor, code_bytes)
-        {
-            Statement::LocalAssignment(variable_declaration)
-        } else if let Some(type_declaration) =
-            TypeDefinition::try_from_node(statement, &mut cursor, code_bytes)
-        {
-            Statement::TypeDefinition(type_declaration)
-        } else if let Some(if_statement) =
-            IfStatement::try_from_node(statement, &mut cursor, code_bytes)
-        {
-            Statement::IfStatement(if_statement)
-        } else if let Some(do_block) = DoBlock::try_from_node(statement, &mut cursor, code_bytes) {
-            Statement::DoBlock(do_block)
-        } else if let Some(generic_for) =
-            GenericFor::try_from_node(statement, &mut cursor, code_bytes)
-        {
-            Statement::GenericFor(generic_for)
-        } else if let Some(numerical_for) =
-            NumericalFor::try_from_node(statement, &mut cursor, code_bytes)
-        {
-            Statement::NumericalFor(numerical_for)
-        } else if let Some(repeat_block) =
-            RepeatBlock::try_from_node(statement, &mut cursor, code_bytes)
-        {
-            Statement::RepeatBlock(repeat_block)
-        } else if let Some(while_loop) =
-            WhileLoop::try_from_node(statement, &mut cursor, code_bytes)
-        {
-            Statement::WhileLoop(while_loop)
-        } else if let Some(set_expression) =
-            SetExpression::try_from_node(statement, &mut cursor, code_bytes)
-        {
-            Statement::SetExpression(set_expression)
-        } else if let Some(compound_set_expression) =
-            CompoundSetExpression::try_from_node(statement, &mut cursor, code_bytes)
-        {
-            Statement::CompoundSetExpression(compound_set_expression)
-        } else if let Some(function_call) =
-            FunctionCall::try_from_node(statement, &mut cursor, code_bytes)
-        {
-            Statement::FunctionCall(function_call)
-        } else if let Some(local_function) =
-            LocalFunction::try_from_node(statement, &mut cursor, code_bytes)
-        {
-            Statement::LocalFunction(local_function)
-        } else if let Some(global_function) =
-            GlobalFunction::try_from_node(statement, &mut cursor, code_bytes)
-        {
-            Statement::GlobalFunction(global_function)
-        } else if let Some(comment) = Comment::try_from_node(statement, &mut cursor, code_bytes) {
-            Statement::Comment(comment)
-        } else {
-            // Should be unreachable, but just to be sure, we won't continue the loop.
-            continue;
-        };
-
         statements.push((
-            token,
+            Statement::from((node.child(0).unwrap(), code_bytes)),
             node.child(1)
                 .map(|node| SingleToken::from((node, code_bytes))),
         ))
