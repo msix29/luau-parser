059fcf9ef6a9c08b49d0f1bc722a5d824fadaa09
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-22 07:43:06 +0300

    Fix `IfStatement.print` function

--- src/display/block/if_statement.rs
+++ src/display/block/if_statement.rs
@@ -1,7 +1,7 @@
 //! Implements helper traits for if statements
 
 use crate::{
-    impl_print_struct,
+    impl_print_struct, optional_print,
     prelude::{ElseIfStatement, ElseStatement, IfStatement},
     print,
 };
@@ -11,7 +11,10 @@ impl_print_struct!(
     { self.if_keyword, print! },
     { self.condition, print! },
     { self.then_keyword, print! },
-    { self.body, print! }
+    { self.body, print! },
+    { self.else_if_expressions, print! },
+    { self.else_expression, optional_print! },
+    { self.end_keyword, print! }
 );
 impl_print_struct!(
     ElseIfStatement,
