e0d3a20fed6568c23545abd05654ea30ff9ace33
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-23 08:28:44 +0300

    Implement missing tratis for local and global functions

--- src/display/function.rs
+++ src/display/function.rs
@@ -1,10 +1,78 @@
 //! Local and global functions.
 
+#[cfg(feature = "raw-values")]
+use super::type_definition::try_generics_to_string;
+#[cfg(feature = "raw-values")]
+use crate::types::HasRawValue;
 use crate::{
-    impl_print_enum, impl_print_struct, optional_print,
-    prelude::{GlobalFunction, GlobalFunctionName, LocalFunction},
-    print,
+    impl_print_enum, impl_print_struct, optional_print, print,
+    types::{GlobalFunction, GlobalFunctionName, LocalFunction},
 };
+
+#[cfg(feature = "raw-values")]
+impl HasRawValue for LocalFunction {
+    fn get_raw_value(&self) -> String {
+        let return_type = if let Some(return_type) = &self.returns {
+            format!(": {}", return_type.get_raw_value())
+        } else {
+            String::new()
+        };
+
+        format!(
+            "function {}{}({}){}",
+            self.function_name.get_raw_value(),
+            try_generics_to_string(&self.generics, false),
+            self.parameters.get_raw_value(),
+            return_type,
+        )
+    }
+}
+
+#[cfg(feature = "raw-values")]
+impl HasRawValue for GlobalFunction {
+    fn get_raw_value(&self) -> String {
+        let return_type = if let Some(return_type) = &self.returns {
+            format!(": {}", return_type.get_raw_value())
+        } else {
+            String::new()
+        };
+
+        format!(
+            "function {}{}({}){}",
+            self.function_name.get_raw_value(),
+            try_generics_to_string(&self.generics, false),
+            self.parameters.get_raw_value(),
+            return_type,
+        )
+    }
+}
+
+#[cfg(feature = "raw-values")]
+impl HasRawValue for GlobalFunctionName {
+    fn get_raw_value(&self) -> String {
+        match self {
+            GlobalFunctionName::SimpleName(name) => name.get_raw_value(),
+            GlobalFunctionName::Table {
+                table,
+                keys,
+                method,
+            } => {
+                let method = if let Some(method) = method {
+                    format!(":{}", method.get_raw_value())
+                } else {
+                    String::new()
+                };
+
+                format!(
+                    "{}{}{}",
+                    table.get_raw_value(),
+                    keys.bare_raw_value_with_separator("."),
+                    method
+                )
+            }
+        }
+    }
+}
 
 impl_print_struct!(
     LocalFunction,
