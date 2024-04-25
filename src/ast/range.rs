797df757e931c6c019fb99fd7966e19316aba939
msix29 <abdulrehmanessam7567@gmail.com>
2024-04-25 10:32:01 +0300

    Add helper functions for `Range`

--- src/ast/range.rs
+++ src/ast/range.rs
@@ -1,6 +1,6 @@
 //! Implements helper traits for [`ranges`](Range).
 
-use crate::prelude::{Range, Position};
+use crate::prelude::{Position, Range};
 
 impl Range {
     /// Create a new [`range`](Range).
@@ -21,5 +21,29 @@ impl Range {
     pub fn offset(&mut self, lines: i32, characters: i32) {
         self.start.offset(lines, characters);
         self.end.offset(lines, characters);
+    }
+
+    /// Turns the range into a [`string`](String), which can be passed to [`from_string`](Self::from_string).
+    pub fn serialize_to_string(&self) -> String {
+        format!(
+            "{},{},{},{}",
+            self.start.line, self.start.character, self.end.line, self.end.character
+        )
+    }
+
+    /// Creates a new [`range`](Range) from the passed [`string`](str).
+    ///
+    /// # Note
+    ///
+    /// This function assumes the string is valid and in the bounds of [`u32`]s. Meaning
+    /// that if the string is malformed, it'll error. To ensure correctness, use
+    /// [`serialize_to_string`](Self::serialize_to_string).
+    pub fn from_string(str: &str) -> Self {
+        let splits = str
+            .split(',')
+            .map(|n| n.parse().unwrap())
+            .collect::<Vec<u32>>();
+
+        Self::new2(splits[0], splits[1], splits[2], splits[3])
     }
 }
