6f4768fa961ea6a3a7b65ad92634264852ff3e33
msix29 <abdulrehmanessam7567@gmail.com>
2025-03-20 15:32:22 +0300

    Add parsing for `Name`

--- src/impl/name.rs
+++ src/impl/name.rs
@@ -1,9 +1,30 @@
-use luau_lexer::prelude::{Lexer, ParseError, Token};
+use std::sync::Arc;
 
-use crate::types::{Name, Parse};
+use luau_lexer::prelude::{Lexer, ParseError, Symbol, Token, TokenType};
+
+use crate::types::{Name, Parse, TypeValue};
 
 impl Parse for Name {
-    fn parse(token: Token, lexer: &mut Lexer, errors: &mut Vec<ParseError>) -> Option<Self> {
-        todo!()
+    fn parse(name: Token, lexer: &mut Lexer, errors: &mut Vec<ParseError>) -> Option<Self> {
+        if !matches!(
+            name.token_type,
+            TokenType::Identifier(_) | TokenType::PartialKeyword(_)
+        ) {
+            return None;
+        }
+
+        maybe_next_token!(lexer, colon, TokenType::Symbol(Symbol::Colon));
+
+        let r#type = if colon.is_some() {
+            TypeValue::parse(lexer.next_token(), lexer, errors).map(Arc::new)
+        } else {
+            None
+        };
+
+        Some(Self {
+            name,
+            colon,
+            r#type,
+        })
     }
 }
