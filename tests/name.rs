d4797520914febab83d2d31e993b6854b3c21473
msix29 <abdulrehmanessam7567@gmail.com>
2025-03-20 15:35:28 +0300

    Add more tests for `Name`

--- tests/name.rs
+++ tests/name.rs
@@ -12,3 +12,35 @@ fn partial_keyword() {
         TokenType::PartialKeyword(PartialKeyword::Type)
     );
 }
+
+#[test]
+fn simple_1() {
+    let mut lexer = Lexer::new("name");
+    let name = Name::parse(lexer.next_token(), &mut lexer, &mut Vec::new());
+
+    assert!(name.is_some());
+    assert_eq!(
+        name.unwrap().name.token_type,
+        TokenType::Identifier("name".to_string())
+    );
+}
+
+#[test]
+fn simple_2() {
+    let mut lexer = Lexer::new("_name");
+    let name = Name::parse(lexer.next_token(), &mut lexer, &mut Vec::new());
+
+    assert!(name.is_some());
+    assert_eq!(
+        name.unwrap().name.token_type,
+        TokenType::Identifier("_name".to_string())
+    );
+}
+
+#[test]
+fn faulty() {
+    let mut lexer = Lexer::new("123");
+    let name = Name::parse(lexer.next_token(), &mut lexer, &mut Vec::new());
+
+    assert!(name.is_none());
+}
