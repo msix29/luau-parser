88974876a1fefc97fa09b15699f8799d1b0fa0b3
msix29 <abdulrehmanessam7567@gmail.com>
2025-03-24 18:00:06 +0300

    Remove useless macros

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -1,23 +1,10 @@
 //! Helpful macros for the lsp
 
 mod call_any;
-mod enum_matches;
 mod get_item_from_enum;
 mod impl_print_enum;
 mod impl_print_struct;
 mod parse_bracketed;
-
-/// Macro used to error when an unhandled variant is met.
-#[macro_export]
-macro_rules! unhandled_kind {
-    ($variable: ident, $currently_parsing: literal) => {{
-        // eprintln!(
-        //     "Reached unhandled kind '{}' when parsing `{}`.",
-        //     $variable, $currently_parsing
-        // );
-        None
-    }};
-}
 
 /// Macro used to error when [`get_range`](crate::types::HasRange::get_range) is called.
 #[macro_export]
