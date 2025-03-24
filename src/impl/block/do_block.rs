0866720a0369d4afdad775253be133c328314adb
msix29 <abdulrehmanessam7567@gmail.com>
2025-03-24 21:45:24 +0300

    Implement `Parse` for `DoBlock`

--- src/impl/block/do_block.rs
+++ src/impl/block/do_block.rs
@@ -1,9 +1,37 @@
-use luau_lexer::prelude::{Lexer, ParseError, Token};
+use luau_lexer::{
+    prelude::{Lexer, ParseError, Token},
+    token::{Keyword, TokenType},
+};
 
-use crate::types::{DoBlock, Parse};
+use crate::types::{Block, DoBlock, Parse, ParseWithArgs};
 
 impl Parse for DoBlock {
-    fn parse(token: Token, lexer: &mut Lexer, errors: &mut Vec<ParseError>) -> Option<Self> {
-        todo!()
+    fn parse(do_keyword: Token, lexer: &mut Lexer, errors: &mut Vec<ParseError>) -> Option<Self> {
+        if do_keyword != TokenType::Keyword(Keyword::Do) {
+            return None;
+        }
+
+        let body = Block::parse_with(
+            lexer.next_token(),
+            lexer,
+            errors,
+            Some(TokenType::Keyword(Keyword::End)),
+        )
+        .unwrap_or_default();
+
+        next_token_recoverable!(
+            lexer,
+            end_keyword,
+            TokenType::Keyword(Keyword::End),
+            TokenType::Keyword(Keyword::End),
+            errors,
+            "Expected <end>"
+        );
+
+        Some(Self {
+            do_keyword,
+            body,
+            end_keyword,
+        })
     }
 }
