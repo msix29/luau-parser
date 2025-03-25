7478cb6fa01b185a0450d02325379a323f3b43f4
msix29 <abdulrehmanessam7567@gmail.com>
2025-03-26 02:39:02 +0300

    Add `TryParse` and `TryParseWith` private traits

--- src/types/traits.rs
+++ src/types/traits.rs
@@ -1,7 +1,6 @@
 //! Module holding all trait definitions in this parser.
 
 use luau_lexer::prelude::{Lexer, ParseError, Token};
-use std::fmt::Debug;
 
 use crate::types::Range;
 
@@ -21,18 +20,53 @@ pub trait Print {
 }
 
 /// A trait that indicates that this struct can be parsed from a [`lexer`](Lexer)
-pub trait Parse<O = Self>: Sized + Debug {
+pub trait Parse<O = Self> {
     fn parse(token: Token, lexer: &mut Lexer, errors: &mut Vec<ParseError>) -> Option<O>;
+}
+pub(crate) trait TryParse<O = Self>
+where
+    O: Parse<O>,
+{
+    fn try_parse(lexer: &mut Lexer, errors: &mut Vec<ParseError>) -> Option<O> {
+        let state = lexer.save_state();
+
+        match O::parse(lexer.next_token(), lexer, errors) {
+            value @ Some(_) => value,
+            None => {
+                lexer.set_state(state);
+
+                None
+            }
+        }
+    }
 }
 
 /// A trait that means this node can be built from a [`tree-sitter Node`](Node).
-pub trait ParseWithArgs<T, O = Self>: Sized {
+pub trait ParseWithArgs<T, O = Self> {
     fn parse_with(
         token: Token,
         lexer: &mut Lexer,
         errors: &mut Vec<ParseError>,
         args: T,
     ) -> Option<O>;
+}
+pub(crate) trait TryParseWithArgs<T, O = Self>
+where
+    O: ParseWithArgs<T, O>,
+{
+    #[inline]
+    fn try_parse_with(lexer: &mut Lexer, errors: &mut Vec<ParseError>, args: T) -> Option<O> {
+        let state = lexer.save_state();
+
+        match O::parse_with(lexer.next_token(), lexer, errors, args) {
+            value @ Some(_) => value,
+            None => {
+                lexer.set_state(state);
+
+                None
+            }
+        }
+    }
 }
 
 /// A trait for getting the range for this specific item.
