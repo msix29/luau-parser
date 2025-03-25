4e360bc1971beb073e66a4c3c4f3fc03fd3ee1a4
msix29 <abdulrehmanessam7567@gmail.com>
2025-03-25 21:05:51 +0300

    Implement `Serialize` and `Deserialize` to `Comment` when `serde` feature is on

--- src/types/block/comment.rs
+++ src/types/block/comment.rs
@@ -2,4 +2,5 @@ use luau_lexer::token::Token;
 
 // A comment.
 #[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub struct Comment(pub Token);
