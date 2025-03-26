ce5fd18b6372fc3f5b3b033a65ec5d40ef9b9650
msix29 <abdulrehmanessam7567@gmail.com>
2025-03-27 00:16:45 +0300

    Implement `Parse` for `RepeatBlock`

--- src/impl/block/repeat_block.rs
+++ src/impl/block/repeat_block.rs
@@ -1,9 +1,49 @@
-use luau_lexer::prelude::{Lexer, ParseError, Token};
+use luau_lexer::{
+    prelude::{Lexer, ParseError, Token},
+    token::{Keyword, TokenType},
+};
 
-use crate::types::{Parse, RepeatBlock};
+use crate::{
+    safe_unwrap,
+    types::{Block, Expression, Parse, Pointer, RepeatBlock, TryParse, TryParseWithArgs},
+    utils::get_token_type_display_extended,
+};
 
 impl Parse for RepeatBlock {
-    fn parse(token: Token, lexer: &mut Lexer, errors: &mut Vec<ParseError>) -> Option<Self> {
-        todo!()
+    fn parse(
+        repeat_keyword: Token,
+        lexer: &mut Lexer,
+        errors: &mut Vec<ParseError>,
+    ) -> Option<Self> {
+        if repeat_keyword != TokenType::Keyword(Keyword::Repeat) {
+            return None;
+        }
+
+        let body = Block::try_parse_with(lexer, errors, TokenType::Keyword(Keyword::End))
+            .unwrap_or_default();
+
+        next_token_recoverable!(
+            lexer,
+            until_keyword,
+            TokenType::Keyword(Keyword::Until),
+            TokenType::Keyword(Keyword::Until),
+            errors,
+            "Expected ".to_string()
+                + get_token_type_display_extended(&TokenType::Keyword(Keyword::Until))
+        );
+
+        let condition = safe_unwrap!(
+            lexer,
+            errors,
+            "Expected <expr>",
+            Pointer::<Expression>::try_parse(lexer, errors)
+        );
+
+        Some(Self {
+            repeat_keyword,
+            body,
+            until_keyword,
+            condition,
+        })
     }
 }
