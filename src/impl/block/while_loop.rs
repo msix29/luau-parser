c8668f7cee2f22c85d3a42753a2b85feffa83dbb
msix29 <abdulrehmanessam7567@gmail.com>
2025-03-29 16:06:15 +0300

    Implement `Parse` for `WhileLoop`

--- src/impl/block/while_loop.rs
+++ src/impl/block/while_loop.rs
@@ -1,9 +1,32 @@
-use luau_lexer::prelude::{Lexer, ParseError, Token};
+use luau_lexer::prelude::{Keyword, Lexer, ParseError, Token, TokenType};
 
-use crate::types::{Parse, WhileLoop};
+use crate::{
+    safe_unwrap,
+    types::{DoBlock, Expression, Parse, Pointer, TryParse, WhileLoop},
+};
 
 impl Parse for WhileLoop {
-    fn parse(token: Token, lexer: &mut Lexer, errors: &mut Vec<ParseError>) -> Option<Self> {
-        todo!()
+    fn parse(
+        while_keyword: Token,
+        lexer: &mut Lexer,
+        errors: &mut Vec<ParseError>,
+    ) -> Option<Self> {
+        if while_keyword != TokenType::Keyword(Keyword::While) {
+            return None;
+        }
+
+        let condition = safe_unwrap!(
+            lexer,
+            errors,
+            "Expected <expr>",
+            Pointer::<Expression>::try_parse(lexer, errors)
+        );
+        let do_block = DoBlock::try_parse(lexer, errors)?;
+
+        Some(Self {
+            while_keyword,
+            condition,
+            do_block,
+        })
     }
 }
