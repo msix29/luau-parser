5451c2dde8f6b94d3adf321e8ca85686058f6ccd
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-04 00:49:14 +0300

    Refactor parse code for `GenericFor`

--- src/impl/block/generic_for.rs
+++ src/impl/block/generic_for.rs
@@ -1,27 +1,23 @@
 use luau_lexer::prelude::{Keyword, Lexer, ParseError, Token, TokenType};
 
-use crate::{
-    types::{DoBlock, GenericFor, List, Parse, TryParse},
-    utils::get_token_type_display_extended,
-};
+use crate::types::{DoBlock, GenericFor, List, Parse, TryParse};
 
 impl Parse for GenericFor {
     fn parse(for_keyword: Token, lexer: &mut Lexer, errors: &mut Vec<ParseError>) -> Option<Self> {
+        let state = lexer.save_state();
+
         if for_keyword != TokenType::Keyword(Keyword::For) {
             return None;
         }
 
         let names = List::try_parse(lexer, errors)?;
 
-        next_token_recoverable!(
-            lexer,
-            in_keyword,
-            TokenType::Keyword(Keyword::In),
-            TokenType::Keyword(Keyword::In),
-            errors,
-            "Expected ".to_string()
-                + get_token_type_display_extended(&TokenType::Keyword(Keyword::In))
-        );
+        maybe_next_token!(lexer, in_keyword, TokenType::Keyword(Keyword::In));
+        let Some(in_keyword) = in_keyword else{
+            lexer.set_state(state);
+
+            return None;
+        };
 
         let expressions = List::try_parse(lexer, errors)?;
         let do_block = DoBlock::try_parse(lexer, errors)?;
