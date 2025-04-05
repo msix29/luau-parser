5e984dc9824968d33723c4c1f34659996ef4955b
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-05 21:32:28 +0300

    Remove unused macros

--- src/impl/macros.rs
+++ src/impl/macros.rs
@@ -1,39 +1,3 @@
-macro_rules! next_token {
-    ($lexer: ident, $name: ident, $pattern: pat, $errors: ident, $error_message: expr) => {
-        next_token_with_condition!(
-            $lexer,
-            $name,
-            matches!(&$name.token_type, $pattern),
-            $errors,
-            $error_message
-        )
-    };
-}
-
-macro_rules! next_token_with_condition {
-    ($lexer: ident, $name: ident, $condition: expr, $errors: ident, $error_message: expr) => {
-        let state = $lexer.save_state();
-        let $name = $lexer.next_token();
-        if !$condition {
-            $errors.push(
-                luau_lexer::prelude::ParseError::new(
-                    state.lexer_position(),
-                    format!(
-                        "{} found {}",
-                        $error_message,
-                        $crate::utils::get_token_type_display(&$name.token_type)
-                    ),
-                    Some(state.lexer_position()),
-                )
-                .into(),
-            );
-            $lexer.set_state(state);
-
-            return None;
-        }
-    };
-}
-
 macro_rules! next_token_recoverable {
     (
         $lexer: ident,
