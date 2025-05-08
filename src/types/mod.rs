483a58a346ff8f1d5840b586ef28be63050b40d5
msix29 <abdulrehmanessam7567@gmail.com>
2025-05-08 16:59:20 +0300

    Add `async` feature and make `Pointer<T>` use `Arc<T>` instead of `Rc<T>` if it's enabled

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -11,7 +11,10 @@
 //! here is for `Statement`, and it isn't exposed directly to consumers of this
 //! crate, but rather through other functions.
 
-use std::rc::Rc;
+#[cfg(not(feature = "async"))]
+use std::rc::Rc as PointerInner;
+#[cfg(feature = "async")]
+use std::sync::Arc as PointerInner;
 
 /// A helper macro to reexport modules.
 macro_rules! reexport {
@@ -25,10 +28,12 @@ reexport!(
     block, bracketed, cst, expression, list, literals, name, traits, value
 );
 
-/// The main pointer used in the [`Cst`]. It's just [`Rc`]. The only reason
-/// this type exists is to allow easily switching to others, like [`Box`] or
-/// [`Arc`](std::sync::Arc) by only editing one line instead of mass refactoring.
-pub type Pointer<T> = Rc<T>;
+/// The main pointer used in the [`Cst`]. It's just [`Rc`](std::rc::Rc)
+/// (or [`Arc`](std::sync::Arc) if "async" feature is enabled). The only reason
+/// this type exists is to allow easily switching to other pointer types, by only
+/// editing one line instead of mass refactoring, and without breaking crates that
+/// use it.
+pub type Pointer<T> = PointerInner<T>;
 
 /// An enum representing printing errors that stopped [`Cst::try_print`] from working.
 #[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
